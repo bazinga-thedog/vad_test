6587c6dee05afcc64ac7b98a20e942c340b38ff2
bmartinho <bruno.m.martinho@gmail.com>
2025-08-01 15:05:04 +0100

    Another

--- lib/flutter_recorder.dart
+++
@@ -1,9 +0,0 @@
-/// Flutter low level audio recorder plugin using miniaudio library and FFI
-library flutter_recorder;
-
-export 'src/audio_data_container.dart';
-export 'src/enums.dart' show PCMFormat, RecorderChannels;
-export 'src/filters/autogain.dart';
-export 'src/filters/echo_cancellation.dart';
-export 'src/filters/filters.dart' show RecorderFilterType;
-export 'src/flutter_recorder.dart';
--- web/compile_init_recorder_module.sh
+++
@@ -1,1 +0,0 @@
-dart compile js -O3 -o init_recorder_module.dart.js ./init_recorder_module.dart
